a5e48f845b03010518094e0230803a2f7a9d8fc6
Aditi Polkam <aditipolkam@gmail.com>
2024-11-21 19:56:33 +0530

    refactor: contract addresses and noode url to be of arb sepolia

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -10,10 +10,10 @@ const settings = {
 };
 export const alchemy = new Alchemy(settings);
 
-export const usdcContractAddress = "0xF8457f287518937962E1E825a07E1a25614D7b95";
+export const usdcContractAddress = "0x3Da67eFCeb61a04E72BE2285caA3f6E77368e80E";
 export const marketContractAddress =
-  "0xff4d7AF6Dd5de6768aD08202a873D1E93ef6112c";
-export const alchemyUrl = `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`;
+  "0xFEaA1A19D7Fc4aA700b428061de8dAA06F85bCC4";
+export const alchemyUrl = `https://arb-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`;
 
 export const provider = new ethers.JsonRpcProvider(alchemyUrl);
 
